d611ca181fd01e05f55f4850f28e3716076a2e55
Eugeniusz Busko <jauchenbusko2gmail.com>
2017-10-03 22:57:03 +0300

    Fourth commit Operations feature are added Unit test of OperationImpl is created
    Created MySQL ATM DB with one table of clients
    Created connection to DB and inserted test user

--- src/main/resources/dbtest.sql
+++ src/main/resources/dbtest.sql
@@ -1,1 +1,10 @@
+CREATE TABLE `ATM`.`clients` ( `id` INT(255) NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+  `surname` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+  `login` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+  `pin` INT(4) NOT NULL , `saldo` INT(255) NOT NULL ,
+  PRIMARY KEY (`id`),
+  UNIQUE (`login`))
+  ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
 INSERT INTO clients VALUES (1, 'Name', 'Surname', 'login', 1111, 50 )
