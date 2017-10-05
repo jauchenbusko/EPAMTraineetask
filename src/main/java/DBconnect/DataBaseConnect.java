6e5d65e01de24f1325bf68bef19611f0662cfe81
Eugeniusz Busko <jauchenbusko@gmail.com>
2017-10-05 11:01:03 +0300

    Fifth  commit Operations feature are added Unit test of OperationImpl is created
    Created MySQL ATM DB with one table of clients
    Created connection to DB and inserted test user
    
    Created DataBaseConnection class
    Connection to ATM DB received successfully
    
    Created function which get client details from DB

--- src/main/java/DBconnect/DataBaseConnect.java
+++ src/main/java/DBconnect/DataBaseConnect.java
@@ -1,5 +1,6 @@
 package DBconnect;
 
+import Operations.OperationsImpl;
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 import java.sql.*;
 import java.sql.Driver;
@@ -9,28 +10,67 @@ import java.sql.SQLException;
 
 public class DataBaseConnect {
 
-    public static final String URL = "jdbc:mysql://localhost:3306/ATM";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/ATM";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
 
+    private String login;
+    private int pin;
 
-    public void dbConnect() throws SQLException {
-        Driver driver = new FabricMySQLDriver();
+    private String name;
+    private String surname;
+    private int saldo;
+
+    private Driver driver = new FabricMySQLDriver();
+    private Connection connection;
+
+    public DataBaseConnect() throws SQLException {
         DriverManager.registerDriver(driver);
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 
-        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        System.out.println(connection.isClosed());
+    public OperationsImpl getClient(String login, int pin) throws SQLException{
+
+        this.login = login;
+        this.pin = pin;
+
+        String queryFirstPart = " SELECT * FROM clients WHERE login = \"";
+        String querySecondPart = "\" AND pin = ";
+        String query = queryFirstPart + login + querySecondPart + pin;
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+
+        if (resultSet == null)
+            System.out.println("empty RS");
+
+        while (resultSet.next()){
+            this.name = resultSet.getString("name");
+            this.surname = resultSet.getString("surname");
+            this.saldo = resultSet.getInt("saldo");
+        }
+
+        return new OperationsImpl(name, surname, login, pin, saldo);
+    }
+
+    public void closeConnection() throws SQLException{
         connection.close();
     }
 
+
+
     public static void main(String args[]){
 
-        DataBaseConnect dataBaseConnect = new DataBaseConnect();
-
         try {
-            dataBaseConnect.dbConnect();
+            DataBaseConnect dataBaseConnect = new DataBaseConnect();
+            OperationsImpl operations = dataBaseConnect.getClient("qwerty", 1234);
+            operations.showClientDetails();
+            dataBaseConnect.closeConnection();
+
         } catch (SQLException e) {
-            System.out.println("Enable to load DB driver");
+            e.printStackTrace();
         }
+
+
     }
 }
