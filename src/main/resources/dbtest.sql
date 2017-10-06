5d9ac4868bf4829ff558975a709c0c1c8e8f0eed
Eugeniusz Busko <jauchenbusko@gmail.com>
2017-10-07 01:42:02 +0300

    Working version ATM class with main method algorithm ready
    TODO refractors in Client And ATM main method

--- src/main/resources/dbtest.sql
+++ src/main/resources/dbtest.sql
@@ -7,4 +7,7 @@ CREATE TABLE `ATM`.`clients` ( `id` INT(255) NOT NULL AUTO_INCREMENT ,
   UNIQUE (`login`))
   ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
 
-INSERT INTO clients VALUES (1, 'Name', 'Surname', 'login', 1111, 50 )
+INSERT INTO clients VALUES (1, 'Name', 'Surname', 'login', 1111, 50 );
+INSERT INTO clients VALUES (2, 'Sergio', 'Ramos', 'qwerty', 1234, 200);
+INSERT INTO clients VALUES (3, 'Iker', 'Casillas', 'sKhy0cvTy', 7946, 1000);
+INSERT INTO clients VALUES (4, 'EPAM', 'TRAINEE', 'EPAM', 1993, 1000);
