dae4d5a15989d6de32be53be044e2a0fe2dbff18
Eugeniusz Busko <jauchenbusko@gmail.com>
2017-10-13 13:26:24 +0300

    Tests update Operations toString(), equals() and hashCode() methods overrided

--- src/main/java/Input/Input.java
+++ src/main/java/Input/Input.java
@@ -1,7 +1,3 @@
-/**
-
- */
-
 package Input;
 
 import java.util.InputMismatchException;
--- src/test/java/DBconnect/DataBaseConnectTest.java
+++ src/test/java/DBconnect/DataBaseConnectTest.java
@@ -19,28 +19,51 @@ public class DataBaseConnectTest {
     private static final int saldo = 1000;
 
     @Test
+    public void connectionTest() throws Exception {
+
+        DataBaseConnect dataBaseConnect = new DataBaseConnect();
+        dataBaseConnect.closeConnection();
+    }
+
+    @Test
     public void checkClientTest() throws Exception {
 
         DataBaseConnect dataBaseConnect = new DataBaseConnect();
         assertEquals(String.valueOf(dataBaseConnect.checkClient(loginOK, pinOK)), "true");
         assertEquals(String.valueOf(dataBaseConnect.checkClient(loginWrong, pinWrong)), "false");
+        dataBaseConnect.closeConnection();
     }
 
     @Test
-    public void getClient() throws Exception {
+    public void getClientTest() throws Exception {
 
         Operations operationsLocal = new Operations(name, surname, loginOK, saldo);
         DataBaseConnect dataBaseConnect = new DataBaseConnect();
         Operations operationsDB = dataBaseConnect.getClient(loginOK, pinOK);
 
         assertEquals(operationsLocal, operationsDB);
-
         boolean boolState = operationsLocal.equals(operationsDB);
         assertEquals(Boolean.toString(boolState), "true" );
+
+        dataBaseConnect.closeConnection();
     }
 
     @Test
-    public void updateClientSaldo() throws Exception {
+    public void updateClientSaldoTest() throws Exception {
+
+        DataBaseConnect dataBaseConnectBeforeUpdate = new DataBaseConnect();
+        dataBaseConnectBeforeUpdate.updateClientSaldo(loginOK, 1000);
+        dataBaseConnectBeforeUpdate.closeConnection();
+
+        Operations operationsLocal = new Operations(name, surname, loginOK, saldo );
+        DataBaseConnect dataBaseConnectAfterUpdate = new DataBaseConnect();
+        Operations operationsDB = dataBaseConnectAfterUpdate.getClient(loginOK, pinOK);
+
+        assertEquals(operationsLocal, operationsDB);
+        boolean boolState = operationsLocal.equals(operationsDB);
+        assertEquals(Boolean.toString(boolState), "true" );
+
+        dataBaseConnectAfterUpdate.closeConnection();
     }
 
 }
